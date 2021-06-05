44d855d92989cf0fcb7de1b155d634a894062a3f
vip <vip@DESKTOP-TVLU5IH>
2021-06-05 19:29:59 +0200

    fix change url of db

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 //initalize mongoose 
-mongoose.connect('mongodb+srv://dbfcis:j4MQgkB76feFW7y@cluster0.a5rkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/SarahahAPI', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://dbfcis:j4MQgkB76feFW7y@cluster0.a5rkl.mongodb.net/SarahahAPI?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 //setup routers
 app.use(require('./routers/user.routes'))
